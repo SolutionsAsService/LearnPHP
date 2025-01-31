e20db9bd9ad61dbd3cfca82b75f2bdce95033a51
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-31 11:37:58 -0800

    Create syntax.php

---
+++ syntax.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php'; ?>
+<section id="syntax">
+    <h2>Basic Syntax of PHP</h2>
+    <p>PHP code is executed on the server, and the result is returned to the browser as plain HTML. Here is an example of a simple PHP script:</p>
+    <pre>
+        <code>
+&lt;?php
+echo "Hello, World!";
+?&gt;
+        </code>
+    </pre>
+    <p>PHP scripts are enclosed in &lt;?php and ?&gt; tags. The echo statement is used to output text to the browser.</p>
+</section>
+<?php include 'includes/footer.php'; ?>
