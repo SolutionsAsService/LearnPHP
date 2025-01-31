1a607eddb9cddb61d09f4d779feefbf6858643bc
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-31 11:37:45 -0800

    Create features.php

---
+++ features.php
@@ -0,0 +1,14 @@
+<?php include 'includes/header.php'; ?>
+<section id="features">
+    <h2>Features of PHP</h2>
+    <ul>
+        <li>Open Source</li>
+        <li>Cross-Platform</li>
+        <li>Easy to Learn and Use</li>
+        <li>Supports a Wide Range of Databases</li>
+        <li>Strong Community Support</li>
+        <li>Extensive Library of Functions</li>
+        <li>Integration with HTML, CSS, and JavaScript</li>
+    </ul>
+</section>
+<?php include 'includes/footer.php'; ?>
