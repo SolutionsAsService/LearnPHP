4899aa84b9ab90bf95e3289106d5a004c4042da8
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-31 11:36:38 -0800

    Create nav.php

---
+++ includes/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="features.php">Features</a></li>
+        <li><a href="syntax.php">Syntax</a></li>
+        <li><a href="benefits.php">Benefits</a></li>
+        <li><a href="examples.php">Examples</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
