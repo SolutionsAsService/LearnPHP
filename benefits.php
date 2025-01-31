b3cb05defb5465fe3515f98d9ee20af37320341f
Noah B <152567394+SolutionsAsService@users.noreply.github.com>
2025-01-31 11:38:11 -0800

    Create benefits.php

---
+++ benefits.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php'; ?>
+<section id="benefits">
+    <h2>Benefits of Using PHP</h2>
+    <ul>
+        <li>Easy to Learn: PHP is straightforward and easy to start with for beginners.</li>
+        <li>Flexible: PHP is highly flexible and can be embedded within HTML.</li>
+        <li>Scalable: PHP can handle large projects and is scalable.</li>
+        <li>Compatible: PHP is compatible with most servers and databases.</li>
+        <li>Community Support: PHP has a large community of developers and extensive documentation.</li>
+    </ul>
+</section>
+<?php include 'includes/footer.php'; ?>
